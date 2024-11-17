2d18f9270d753dd2bbc992b0430b1b2437e5fff0
quantum <ttherealchaos@gmail.com>
2024-11-16 16:47:55 -0800

    added font, text, and instructions reworked vertex placement to support all polygons added custom input for ratio option changed colors to be based off polygon size

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -4,9 +4,6 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include <cstdlib> 
-#include <cmath>
-#include <ctime>
 
 //Make the code easier to type with "using namespace"
 using namespace sf;
@@ -14,110 +11,226 @@ using namespace std;
 
 int main()
 {
-	srand(static_cast<unsigned int>(time(0)));
+	/***PRE LOOP VARIABLES***/
+
 	// Create a video mode object
-	VideoMode vm(1920, 1080);
+	VideoMode vm(1500, 900);
 	// Create and open a window for the game
-	RenderWindow window(vm, "Chaos Game!!", Style::Default);
-	
+	RenderWindow window(vm, "Chaos Game :D", Style::Default);
+	//////////////////////////////////////////
+
+
+	// loads a font, end program if not found
+	Font lol;
+	if (!lol.loadFromFile("Mojangles.ttf"))
+	{
+		cout << "poopoo caca" << endl;
+		return 0;
+	}
+	//////////////////////////////////////////
+
+
+	// create text objects and set their font and font size
+	Text text, customRatioText;
+	text.setFont(lol);
+	text.setString("Left click to place your vertices,\nRight click to start drawing.\n\nTo select a custom ratio, press r, type a float, and enter.");
+	text.setCharacterSize(32);
+
+	customRatioText.setFont(lol);
+	customRatioText.setCharacterSize(32);
+	//////////////////////////////////////////
+
+
+	// create and initialize variables for while loop
+	int maxDistance = -1;
+	float ratio = -1;
+	bool RMBPressed = false;
+	string input;
+
 	vector<Vector2f> vertices;
 	vector<Vector2f> points;
+	//////////////////////////////////////////
 
+
+	/*
+	****************************************
+	Game loop
+	****************************************
+	*/
 	while (window.isOpen())
 	{
 		/*
 		****************************************
-		Handle the players input
+		Input handling
 		****************************************
 		*/
 		Event event;
 		while (window.pollEvent(event))
 		{
-		    if (event.type == Event::Closed)
-		    {
-					// Quit the game when the window is closed
-					window.close();
-		    }
-		    if (event.type == sf::Event::MouseButtonPressed)
-		    {
-			if (event.mouseButton.button == sf::Mouse::Left)
+			// Quit the game when the window is closed
+			if (event.type == Event::Closed)
 			{
-			    std::cout << "the left button was pressed" << std::endl;
-			    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-			    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-	
-			    if(vertices.size() < 3)
-			    {
-				vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-			    }
-			    else if(points.size() == 0)
-			    {
-				///fourth click
-				///push back to points vector
-					points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-			    }
+
+				window.close();
 			}
-		    }
-			if (event.type == sf::Event::KeyPressed)
+
+
+			// mouse buttons
+			else if (event.type == Event::MouseButtonPressed)
 			{
-				if (event.key.code == Keyboard::R)
+				input = "";
+				// left click
+				if (event.mouseButton.button == Mouse::Left)
 				{
-					// Reset the game
-					vertices.clear();
-					points.clear();
+					cout << "LMB: " << event.mouseButton.x << " " << event.mouseButton.y << endl;
+
+					// only set vertices before right mouse button is pressed
+					if (!RMBPressed)
+					{
+						vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					}
+				}
+
+
+				// right click
+				else if (event.mouseButton.button == Mouse::Right && vertices.size() > 0)
+				{
+					// only does this stuff the first time right mouse button is pressed
+					if (RMBPressed == false)
+					{
+						// push back the last placed vertix
+						points.push_back(vertices[vertices.size() - 1]);
+
+						// defaults ratio values depending on vertix count (only sets if a custom value was not set)
+						if (ratio == -1)
+						{
+							ratio = 0.8;
+							switch (vertices.size())
+							{
+							case 3:
+								ratio = 0.5;
+								break;
+							case 4:
+								ratio = 0.6666667;
+								break;
+							case 5:
+								ratio = 0.618;
+								break;
+							case 6:
+								ratio = 0.667;
+								break;
+							case 7:
+								ratio = 0.692;
+								break;
+							case 8:
+								ratio = 0.707;
+								break;
+							case 9:
+								ratio = 0.742;
+								break;
+							case 10:
+								ratio = 0.764;
+								break;
+							}
+						}
+
+
+						// finds the distance between the two farthest points
+						int distance;
+						for (int i = 0; i < vertices.size(); ++i)
+						{
+							for (int j = 0; j < vertices.size(); ++j)
+							{
+								distance = sqrt(pow((vertices[i].x - vertices[j].x), 2) + pow((vertices[i].y - vertices[j].y), 2));
+								if (maxDistance < distance)
+									maxDistance = distance;
+							}
+						}
+						cout << "Max distance: " << maxDistance << endl;
+
+					}
+
+					RMBPressed = true; // once right mouse button is pressed, game moves on to drawing phase and you can no longer edit things
+					cout << "RMB: " << event.mouseButton.x << " " << event.mouseButton.y << endl;
+
 				}
 			}
+
+			// takes in and parses text input
+			if (event.type == Event::TextEntered)
+			{
+				input += static_cast<char>(event.text.unicode);
+				cout << input << endl;
+				string str = "\n\n\n\nEnter float: " + input.substr(1);
+				if (tolower(input[0]) == 'r') customRatioText.setString(str);
+			}
 		}
+
+		// close when esc hit
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
+
+		// sets custom ratio when enter is hit
+		if (Keyboard::isKeyPressed(Keyboard::Enter) && !RMBPressed)
+		{
+			if (tolower(input[0]) == 'r' && (isdigit(input[1]) || input[1] == '.' && isdigit(input[2])))
+			{
+				ratio = stof(input.substr(1));
+				cout << ratio << endl;
+			}
+			input = "";
+			string temp = "\n\n\n\n" + to_string(ratio);
+			customRatioText.setString(temp);
+		}
+		//////////////////////////////////////////
+
+
+
 		/*
 		****************************************
 		Update
 		****************************************
 		*/
-	
-		if(points.size() > 0)
+
+		// set points if right mouse button is pressed
+		if (RMBPressed)
 		{
-		    ///generate more point(s)
-		    ///select random vertex
-		    ///calculate midpoint between random vertex and the last point in the vector
-		    ///push back the newly generated coord.
-			for (int i = 0; i < 5; ++i) // Generate 5 points per frame for faster visualization
+			for (int i = 0; i < vertices.size() * 3; ++i)
 			{
-				// Select a random vertex
-				int randomIndex = rand() % vertices.size();
-				// Calculate the midpoint between the random vertex and the last point
-				Vector2f lastPoint = points.back();
-				Vector2f randomVertex = vertices[randomIndex];
-				Vector2f newPoint((lastPoint.x + randomVertex.x) / 2, (lastPoint.y + randomVertex.y) / 2);
-				// Push back the newly generated point
-				points.push_back(newPoint);
+				int randomPoint = rand() % vertices.size();
+
+				Vector2f newpoint = { (vertices[randomPoint].x - points[points.size() - 1].x) * ratio + points[points.size() - 1].x,
+									  (vertices[randomPoint].y - points[points.size() - 1].y) * ratio + points[points.size() - 1].y };
+				points.push_back(newpoint);
 			}
 		}
-	
+
+
+
 		/*
 		****************************************
 		Draw
 		****************************************
 		*/
 		window.clear();
-		
-		for(size_t i = 0; i < vertices.size(); i++)
+
+		// draw vertices
+		for (int i = 0; i < vertices.size() && !RMBPressed; ++i)
 		{
-		    RectangleShape rect(Vector2f(10,10));
-		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-			rect.setPosition(vertices[i]);
+			RectangleShape rect(Vector2f(10, 10));
+			rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
 			rect.setFillColor(Color::Blue);
-		    window.draw(rect);
+			window.draw(rect);
 		}
+
+		// draw points
 		for (const auto& point : points)
 		{
-			RectangleShape rect(Vector2f(2, 2)); // Smaller points
+			RectangleShape rect(Vector2f(2, 2));
 			rect.setPosition(point);
 			rect.setFillColor(Color::Red);
-			window.draw(rect);
 			float minDistance = std::numeric_limits<float>::max();
 			for (const auto& vertex : vertices)
 			{
@@ -129,14 +242,21 @@ int main()
 			}
 
 			// Change color based on distance
-			if (minDistance < 50)
+			if (minDistance < maxDistance / 7)
 				rect.setFillColor(Color::Red);
-			else if (minDistance < 100)
+			else if (minDistance < maxDistance / 4)
 				rect.setFillColor(Color::Yellow);
 			else
 				rect.setFillColor(Color::Green);
 
 			window.draw(rect);
+		}
+
+		// draw text
+		if (!RMBPressed)
+		{
+			window.draw(text);
+			window.draw(customRatioText);
 		}
 
 		window.display();
